b35070e1265ba8caffef066ceb40629dd496e9ea
Robertas Tutl <robertastut@gmail.com>
2019-03-26 00:20:46 +0100

    Commit changes

---
+++ MatrixHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace SubMAtrix
+{
+    public class MatrixHelper
+    {
+        internal static int[,] Create2DArray(string textPath, int matrixSize)
+        {
+            int[,] twoDimArray = new int[matrixSize, matrixSize];
+
+            using (StreamReader reading = new StreamReader(textPath))
+            {
+                while (!reading.EndOfStream)
+                {
+                    string line = reading.ReadLine();
+
+                    if (line.Length > 3) // working with matrix until 99x99
+                    {
+                        for (int i = 0; i < matrixSize; i++)
+                        {
+                            for (int j = 0; j < matrixSize; j++)
+                            {
+                                twoDimArray[i, j] = int.Parse(line.Split(' ')[j]);
+                            }
+                            line = reading.ReadLine();
+                        }
+                    }
+                }
+                return twoDimArray;
+            }
+
+        }
+        public static void printMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+        internal static void PrintMatrixWithMax2x2(int[,] twoDimArray, int positionIndexI, int positionIndexJ)
+        {
+            int matrixSize = twoDimArray.GetLength(0);
+            int count = 0;
+            int indexInPrintSequence = positionIndexI * matrixSize + positionIndexJ;
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    if (count == indexInPrintSequence || count == indexInPrintSequence + 1 ||
+                        count == indexInPrintSequence + matrixSize || count == indexInPrintSequence + matrixSize + 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.Write(twoDimArray[i, j] + " ");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.Write(twoDimArray[i, j] + " ");
+                    }
+                    count++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace SubMAtrix
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                string textPath = "text.txt";
+                int matrixSize = 0;
+
+                using (StreamReader firstLine = new StreamReader(textPath))
+                {
+                    matrixSize = int.Parse(firstLine.ReadLine());
+                }
+
+                int[,] twoDimArray = MatrixHelper.Create2DArray(textPath, matrixSize);
+
+                Console.WriteLine($"Matrix in {textPath} file:\n ");
+
+                MatrixHelper.printMatrix(twoDimArray);
+
+                int max = int.MinValue;
+                int positionIndexI = 0;
+                int positionIndexJ = 0;
+
+                for (int i = 0; i < matrixSize - 1; i++)
+                {
+                    for (int j = 0; j < matrixSize - 1; j++)
+                    {
+                        if (twoDimArray[i, j] + twoDimArray[i, j + 1]
+                                              + twoDimArray[i + 1, j]
+                                              + twoDimArray[i + 1, j + 1]
+                                              > max)
+                        {
+                            max = twoDimArray[i, j] + twoDimArray[i, j + 1] + twoDimArray[i + 1, j] + twoDimArray[i + 1, j + 1];
+                            positionIndexI = i;
+                            positionIndexJ = j;
+                        }
+                    }
+
+                }
+
+                Console.WriteLine("\nSub matrix 2 x 2\n ");
+
+                MatrixHelper.PrintMatrixWithMax2x2(twoDimArray, positionIndexI, positionIndexJ);
+
+                Console.WriteLine("\nMaximal sum 2x2 : {0}\n", max);
+            }
+        
+    }
+}
